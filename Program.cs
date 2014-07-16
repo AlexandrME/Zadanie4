2834a790e85b25a53bd48d5533f209db0c253921
Alexandr <manaev-2012@mail.ru>
2014-07-16 12:35:13 +0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,29 +1,47 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace Практика_4___пробник_2__
+namespace Практика
 {
     class Program
     {
         static void Main(string[] args)
         {
+            int d = 0;
             Console.WriteLine("Введите строку:");
             string s = Console.ReadLine();
+             
             s = s.Trim();
-            s = s.Replace(" ", ""); 
-            for (int i = 0; i < s.Length; i++)
+            s = s.Replace(" ", "");
+        
+            int i=0;
+            int j = s.Length-1;
+            do
             {
-                for (int j = s.Length - 1; j > 0; j--)
+                if (s[i] == s[j])
                 {
-                    if (s[i] == s[j])
-                        Console.WriteLine("Введенная строка - палиндром");
-                    else
-                        Console.WriteLine("Введенная строка - не палиндром");
+                    i++;
+                    j--;
+                    d++;
+                }
+                else
+                {
+                    Console.WriteLine("Введенная строка - не палиндром");
                     Console.ReadLine();
                 }
             }
+            while ( i < s.Length);
+            if (d == s.Length )
+                Console.WriteLine("Введенная строка - палиндром");
+            else
+                Console.WriteLine("Введенная строка - не палиндром");
+            
+        
+            Console.ReadLine();
+
         }
+
     }
 }
